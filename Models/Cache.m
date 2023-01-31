f68758f785513e25cd4d392298f6b5cbfd8f01b8
Karl Öqvist <karlo@centiro.se>
2023-01-31 16:22:09 +0100

    Removed debug code and general cleanup

--- Models/Cache.m
+++ Models/Cache.m
@@ -1,7 +1,3 @@
-parameter_names = ["cache_size", "cache_size_2", "test"];
-
-parameter_values = {};
+parameter_names = ["cache_size"];
 
 parameter_values{1} = [8,16,24,32,64,128,256,512,1024,1440,2048,2500,3200,3800,4096,5200,5800,6400,6800,7200];
-parameter_values{2} = 1:20;
-parameter_values{3} = [0, 1, 2];
--- Scripts/SweepingAutoRun.m
+++ Scripts/SweepingAutoRun.m
@@ -2,25 +2,23 @@
 % Volvo Architecture Component-Blocks 
 
 blocks_with_parameter = ["Cache"];
-pva(1:20) = -1;
 p_name = [];
-pva_cell = {};
+pva = {};
 
 for i = 1:length(blocks_with_parameter)
-    blockname = blocks_with_parameter(i);
-    script = blockname + ".m";
+    script = blocks_with_parameter(i) + ".m";
     run(script);
 
     p_name = [p_name, parameter_names];
 
     for j = 1:length(parameter_values)
         index = j+i-1;
-        pva_cell{index} = parameter_values{j};
+        pva{index} = parameter_values{j};
     end
 end
 
 % Get all permutations of the vectors using the ndgrid function
-[X{1:numel(pva_cell)}] = ndgrid(pva_cell{:});
+[X{1:numel(pva)}] = ndgrid(pva{:});
 
 % Reshape the permutations into a matrix
 master_pva = cell2mat(cellfun(@(x) x(:), X, 'UniformOutput', false));
