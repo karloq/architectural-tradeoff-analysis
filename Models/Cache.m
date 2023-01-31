efa5b227247d3f5b467257ebaf5e2a40512ad418
Karl Öqvist <karlo@centiro.se>
2023-01-31 16:10:09 +0100

    Added model script for cache and sweep script

---
+++ Models/Cache.m
@@ -0,0 +1,7 @@
+parameter_names = ["cache_size", "cache_size_2", "test"];
+
+parameter_values = {};
+
+parameter_values{1} = [8,16,24,32,64,128,256,512,1024,1440,2048,2500,3200,3800,4096,5200,5800,6400,6800,7200];
+parameter_values{2} = 1:20;
+parameter_values{3} = [0, 1, 2];
