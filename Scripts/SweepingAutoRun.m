6030f7abf7f0a19758670a0d61b54f1ab95028e9
Karl Öqvist <karlo@centiro.se>
2023-02-08 08:04:17 +0100

    Completed AutoRunScript

--- Scripts/SweepingAutoRun.m
+++ Scripts/SweepingAutoRun.m
@@ -7,7 +7,14 @@
 model_file = 'DebuggingModel_1.slx';
 model_name = 'DebuggingModel_1/';
 % Blocks (that have parameters) in the model
-blocks_with_parameter = ["Cache"]; 
+blocks = ["Cache", "C3","Container", "Lambda"]; 
+blocks_zeroes = [3];
+% Must follow the order of blocks and the parameter order inside
+out_frame = ["cache_size", "consistency", "no_cores", ...
+    "container_no_instances","ram","lambda_no_instances", "cost", "time"];
+out_frame_zeroes = [3,4,5,7,8];
+out_frame_cost_index = 7;
+out_frame_time_index = 8;
 % Suppress warnings
 %#ok<*NBRAK2> 
 %#ok<*AGROW> 
@@ -17,6 +24,8 @@ blocks_with_parameter = ["Cache"];
 
 % Vector to contain all parameter names
 p_name = [];
+% Vector to contain all block names
+b_name = [];
 % Matrix to contain all possible parameter values (per value) from each
 % block. Will be overwritten for each iteration.
 pva = {};
@@ -28,15 +37,25 @@ master_simulation_data = [];
 %-------------------- Simulation setup creation -------------------------%
 
 % Read all "blockname.m" files and iteratively populate pva and p_name
-for i = 1:length(blocks_with_parameter)
-    script = blocks_with_parameter(i) + ".m";
+column = 0;
+for i = 1:length(blocks)
+    % If block is not in current model, skip it
+    if ismember(i,blocks_zeroes)
+        continue
+    end
+
+    column = column + 1;
+    script = blocks(i) + ".m";
+
+    % Run script and fill workspace with variables
     run(script);
 
-    p_name = [p_name, parameter_names]; 
+    p_name = [p_name, parameter_names];
 
     for j = 1:length(parameter_values)
-        index = j+i-1;
+        index = j+column-1;
         pva{index} = parameter_values{j};  
+        b_name = [b_name, blocks(i)];
     end
 end
 
@@ -49,16 +68,48 @@ master_pva = cell2mat(cellfun(@(x) x(:), X, 'UniformOutput', false));
 %--------------------------- Simulation ---------------------------------%
 
 % Simulation loop
+% Get size of the parameters to use
 sz = size(master_pva);
+% Get size for all parameters there is in the output frame
+out_frame_sz = size(out_frame);
+
 for runs = 1:sz(1)
-    for params = 1:sz(2)
-    % Load parameters
-    set_param(model_name + blocks_with_parameter, p_name(params),int2str(master_pva(runs,params)));
+    out_frame_row = zeros(1,out_frame_sz(2));
+    param_index = 0;
+    % For all parameters in output frame
+    for out_params = 1:(out_frame_sz(2))
+        % Keep separate idex for parameters to use
+        param_index = param_index + 1;
+        % If parameter is not in model, skip it
+        if ismember(out_params,out_frame_zeroes)
+            param_index = param_index - 1;
+            continue;
+        end
+        % Get parameter value
+        parameter_value = master_pva(runs,param_index);
+        % Save Parameter value to output frame
+        out_frame_row(out_params) = parameter_value;
+        % Load parameter value to model
+        set_param(model_name + b_name(param_index), p_name(param_index), ...
+            int2str(parameter_value));
     end
+    
+    % Reset cost and times
+    c3_sout = [0,0];
+    lambda_sout = [0,0];
+    container_sout = [0,0];
+
     % Run simulation
     sim_data = sim(model_file);
-    % Collect data
-    % TODO
+
+    % Sum quality measures
+    out_frame_row(out_frame_time_index) = c3_sout(1,1) + lambda_sout(1,1) ...
+        + container_sout(1,1);
+    out_frame_row(out_frame_cost_index) = c3_sout(1,2) + lambda_sout(1,2) ...
+        + container_sout(1,2);
+    
+    % Add collected row to output frame
+    out_frame = [out_frame; out_frame_row];
 end
 
 
